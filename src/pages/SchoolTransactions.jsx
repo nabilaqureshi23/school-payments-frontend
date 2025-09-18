879264ef0df8442c946d40ba31c6252dd9292b92
Nabila <127545408+nabilaqureshi23@users.noreply.github.com>
2025-09-18 13:00:25 +0530

    Update SchoolTransactions.jsx

--- src/pages/SchoolTransactions.jsx
+++ src/pages/SchoolTransactions.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000"; // backend
+const API_BASE_URL = import.meta.env.VITE_API_URL||"http://localhost:3000";
 
 function SchoolTransactions() {
   const [schools, setSchools] = useState([]);
